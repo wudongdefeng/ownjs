2af05f17b70148249f8a9b13c4eebab4ab33a687
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-06-04 13:18:26 +0800

    Update cat.sh

--- scripts/cat.sh
+++ scripts/cat.sh
@@ -5,7 +5,7 @@ cd tem
 
 curl --connect-timeout 10 -O https://gitea.com/wkyilisha/m/raw/branch/main/ff.json || echo "Link ff doesn't exist"   
 curl --connect-timeout 10 -O http://www.mikkk.xyz/m.json || echo "Link m doesn't exist"   
-
+curl --connect-timeout 10 -O https://notabug.org/laoo1976/cat/raw/master/mao.txt || echo "Link ff doesn't exist"   
 grep -lrIZ '404 Not Found'. | xargs -0 rm -rf --
 grep -lrIZ '失败'. | xargs -0 rm -rf --
 
