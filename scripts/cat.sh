ab268ff520d5873a2ce26077b6395cd9f672308e
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-06-04 12:49:44 +0800

    Create cat.sh

---
+++ scripts/cat.sh
@@ -0,0 +1,20 @@
+cd ./wind
+mkdir tem
+cd tem
+
+
+curl --connect-timeout 10 -O https://gitea.com/wkyilisha/m/raw/branch/main/ff.json || echo "Link ff doesn't exist"   
+curl --connect-timeout 10 -O http://www.mikkk.xyz/m.json || echo "Link m doesn't exist"   
+
+grep -lrIZ '404 Not Found'. | xargs -0 rm -rf --
+grep -lrIZ '失败'. | xargs -0 rm -rf --
+
+grep -rl . -e '失败' | xargs rm --
+cp -r *.* ..
+cd ..
+rm -rf tem
+
+
+
+
+
