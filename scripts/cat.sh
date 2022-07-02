cc507c5fdc28685cc553768a971eec73c8e59e83
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-07-03 07:10:06 +0800

    Update cat.sh

--- scripts/cat.sh
+++ scripts/cat.sh
@@ -18,10 +18,11 @@ curl --connect-timeout 10 -O http://52xcl.cn/m2.txt || echo "Link m2 doesn't exi
 curl --connect-timeout 10 -O http://fangcun.atwebpages.com/mao.json || echo "Link ff doesn't exist"   
 
 curl --connect-timeout 10 -o m1.json http://xiaoguozitv.cn/m.json || echo "Link m1 doesn't exist"   
-
+curl --connect-timeout 10 -O https://freed.yuanhsing.cf/TVBox/meowcf.json || echo "Link meowcf doesn't exist"
 curl --connect-timeout 10 -O http://xiaoguozitv.cn/redbull.json || echo "Link redbull doesn't exist"   
 curl --connect-timeout 10 -O https://gitea.com/yirenxing/TVBOX/raw/branch/main/BOX/xinmao.json || echo "Link xinmao doesn't exist"   
-curl --connect-timeout 10 -O https://github.com/liu673cn/biubiu/raw/main/bb.txt || echo "Link xinmao doesn't exist"   
+curl --connect-timeout 10 -O https://github.com/liu673cn/biubiu/raw/main/bb.txt || echo "Link xinmao doesn't exist"
+
 grep -lrIZ '404 Not Found'. | xargs -0 rm -rf --
 grep -lrIZ '失败'. | xargs -0 rm -rf --
 
